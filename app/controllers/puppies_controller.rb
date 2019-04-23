a7307e37181b61be181da03b28ac727caf51835b
github username <github email address>
2019-04-23 16:16:44 -0500

    Done.

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -1,3 +1,54 @@
 class PuppiesController < ApplicationController
-   
+  
+#see all puppies
+    get '/puppies' do
+       @puppy = Puppy.all
+          erb :view
+    end
+
+
+#create a new puppy
+
+get '/puppies/new' do
+    
+    erb :new
+end
+
+
+get '/puppies/:id' do
+    @puppy = current_puppy
+    erb :show
+end
+
+post '/puppies' do
+    Puppy.create(params)
+    redirect '/puppies'
+end
+
+get '/puppies/:id/edit' do
+    @puppy = current_puppy
+    erb :edit
+end
+
+ #edit an existing puppy
+
+ patch '/puppies/:id' do
+    puppy = current_puppy
+    puppy.update(name: params[:name], breed: params[:breed])
+    redirect "/puppies/#{params[:id]}"
+ end
+
+ 
+ #delete an existing puppy
+
+ delete '/puppies/:id' do
+    puppy = current_puppy
+    puppy.destroy
+    redirect "/puppies"
+ end
+
+def current_puppy
+    Puppy.find(params[:id]) 
+end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,11 @@ Puppy.destroy_all
 20.times do
 Puppy.create(name: Faker::Creature::Dog.name , breed: Faker::Creature::Dog.breed)
 end
+
+
+
+
+
+
+
+#destroy all the data
